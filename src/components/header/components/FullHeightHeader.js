e9d1677772652cf2d1668326410563bcc3613747
Gustavo Ennes <gustavo@ennes.dev>
2022-12-22 18:10:46 -0300

    Hover menu effect

--- src/components/header/components/FullHeightHeader.js
+++ src/components/header/components/FullHeightHeader.js
@@ -2,6 +2,7 @@ import { Grid, styled } from "@mui/material";
 
 export const FullHeightHeader = styled(Grid)(({ theme: { breakpoints } }) => ({
   height: "10vh",
+  backgroundColor: "rgba(255, 255, 255, 0.5)",
   [breakpoints.up("md")]: {
     height: "100vh",
   },
--- src/components/header/components/HeaderItem.js
+++ src/components/header/components/HeaderItem.js
@@ -4,18 +4,18 @@ import { useTransition } from "../../../context/TransitionContext";
 
 const StyledGrid = styled(Grid)(() => ({
   height: "calc(100vh / 6)",
-  backgroundColor: `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${
-    Math.random() * 255
-  }, 0.3)`,
 }));
 
 const StyledButton = styled(Button)(() => ({
   height: "100%",
   width: "100%",
-}))
+  color: "white",
+  fontSize: "10px",
+}));
 
 export const HeaderItem = ({ title }) => {
   const { component, setComponent, setOldComponent } = useTransition();
+  const spanId = `span${title}`;
 
   const handleClick = () => {
     if (title !== component) {
@@ -24,9 +24,36 @@ export const HeaderItem = ({ title }) => {
     }
   };
 
+  const handleMouseEnter = () => {
+    console.log("mouse enters");
+    document
+      .getElementById(spanId)
+      ?.classList.add(
+        "animate__animated",
+        "animate__jello",
+        "animate__duration__200ms"
+      );
+  };
+  const onMouseLeave = () => {
+    console.log("mouse leaves");
+    document
+      .getElementById(spanId)
+      ?.classList.remove(
+        "animate__animated",
+        "animate__jello",
+        "animate__duration__200ms"
+      );
+  };
+
   return (
     <StyledGrid item xs>
-      <StyledButton onClick={handleClick}>{title}</StyledButton>
+      <StyledButton
+        onClick={handleClick}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        <span id={spanId}>{title}</span>
+      </StyledButton>
     </StyledGrid>
   );
 };
