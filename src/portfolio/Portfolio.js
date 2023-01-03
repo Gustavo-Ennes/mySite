f8d61e1d0b6aa266640bd4665473701f35e4565c
Gustavo Ennes <gustavo@ennes.dev>
2023-01-03 15:33:51 -0300

    Add sections base

---
+++ src/portfolio/Portfolio.js
@@ -0,0 +1,16 @@
+import { Grid, Typography, styled } from "@mui/material";
+import { MainFrameContainer } from "../components/mainFrame/MainFrameContainer";
+import { ThinLayerGrid } from "../components/ThinLayerGrid";
+
+const StyledTypography = styled(Typography)(() => ({
+  verticalAlign: "middle",
+  padding: "35vh 0 0 0",
+}));
+
+export const Portfolio = () => (
+  <MainFrameContainer disableGutters>
+    <ThinLayerGrid item xs={12} color="blue" >
+      asd
+    </ThinLayerGrid>
+  </MainFrameContainer>
+)
